d74ba1e4ddc405415ff73310a4a6c26e72c1ea1e
Meet Patel <mpate150@stevens.edu>
2024-10-16 23:14:52 -0500

    resume upd

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -59,7 +59,7 @@ const Heroo = () => {
                     <img src='/prof3.jpg' alt='Profile Image' className='rounded-xl p-2'/>
                 </Button>
                 {/* <Image 
-                  src='/prof3.jpg' 
+                  src='/prof.jpg' 
                   alt='Profile Image' 
                   layout='fill'
                   objectFit='cover'
@@ -115,7 +115,7 @@ const Heroo = () => {
                     position='right'
                   />    
                 </a> 
-                <a href="/Meet_Patel_Resume.pdf">
+                <a href="/resume.pdf">
                   <MagicButton
                     title='View Resume'
                     icon={<FaLocationArrow/>}
